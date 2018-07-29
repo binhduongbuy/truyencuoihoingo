db5f76c07edfa6196af467702e4eb3aefa0238c7
Truong Dung <34378419+binhduongbuy@users.noreply.github.com>
2018-07-30 01:00:49 +0700

    1

--- components/SideBar/index.jsx
+++ components/SideBar/index.jsx
@@ -8,15 +8,7 @@ class SideBar extends React.Component {
 
         return (
             <div className='sidebar'>
-                <Youtube {...this.props} />
-		<div className='sidebar__sponsor'>Sponsor</div>
-                <div className='sidebar__title'>Important</div>
-		<div className='sidebar__link'>
-		<a href='/about/'>Giới thiệu Mi Xao Bo</a>
-		<a href='http://thaoam.com'>Phát triển giao diện Thảo Am</a>
-		<a href='/payments/'>How to pay? Thanh toán</a>
-		<a href='/help'>Hướng dẫn chọn món ăn</a>
-		</div>
+               
             </div>
         );
     }
